7dd9fb99b1f62cbb226563b6e133b85b18623483
BlackoJack <billing@sibdata.ru>
2016-03-10 01:33:24 +0600

    Refactoring3

--- modules/main/controllers/DefaultController.php
+++ modules/main/controllers/DefaultController.php
@@ -2,9 +2,7 @@
 
 namespace app\modules\main\controllers;
 
-use app\modules\main\models\ContactForm;
 use yii\web\Controller;
-use Yii;
 
 class DefaultController extends Controller
 {
@@ -20,32 +18,5 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
-    }
-}
-
-class ContactController extends Controller
-{
-    public function actions()
-    {
-        return [
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
-    public function actionIndex()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        } else {
-            return $this->render('index', [
-                'model' => $model,
-            ]);
-        }
     }
 }
