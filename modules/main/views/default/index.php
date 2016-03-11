ec11191d40c900cf12703a95e42540affee114cc
BlackoJack <billing@sibdata.ru>
2016-03-11 17:52:11 +0600

    Начал заполнение

--- modules/main/views/default/index.php
+++ modules/main/views/default/index.php
@@ -38,14 +38,11 @@ $this->title = Yii::$app->name;
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Думаете слишком сложно?</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Нет ничего проще в нашей форме подачи заявки. Для каждого индивидуальный подход. Можете описать вашу проблему в 3 словах, а можете, благодаря нашему сервису, составить полный план обслуживания и узнать приблизительную стоимость. В любом случае наш менеджер свяжется с вами для уточнения всех деталей</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="/doc">Как это работает? &raquo;</a></p>
             </div>
         </div>
 
