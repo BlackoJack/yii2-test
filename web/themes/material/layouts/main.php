6014377af2bb280faf79f6943dca890d23fbcc24
BlackoJack <billing@sibdata.ru>
2016-03-11 16:10:56 +0600

    переделал Меню

--- web/themes/material/layouts/main.php
+++ web/themes/material/layouts/main.php
@@ -41,9 +41,6 @@ AppAsset::register($this);
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
     ]);
 
             echo Nav::widget([
