a7f6dfb05c67cf8ca88b7b936bf6405c4f631d05
BlackoJack <billing@sibdata.ru>
2016-03-11 17:40:16 +0600

    переделал Меню

--- modules/main/views/default/index.php
+++ modules/main/views/default/index.php
@@ -9,9 +9,9 @@ $this->title = Yii::$app->name;
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">IT-решение для решения ваших задач.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="/login">Подать заявку на обслуживание</a></p>
     </div>
 
     <div class="body-content">
