927dcf1149d7ae87b1fa1202e08813874db5ce99
BlackoJack <billing@sibdata.ru>
2016-03-11 17:25:26 +0600

    переделал Меню

--- web/themes/material/layouts/main.php
+++ web/themes/material/layouts/main.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
   <?php $this->beginBody() ?>
   <nav class="light-blue lighten-1" role="navigation">
     <div class="container">
-      <div class="nav-wrapper"><a id="logo-container" href="#" class="brand-logo"><?php echo Html::encode(\Yii::$app->name); ?></a>
+      <div class="nav-wrapper"><a id="logo-container" href="/main/default/index" class="brand-logo"><?php echo Html::encode(\Yii::$app->name); ?></a>
 	  		<?php
 						echo Menu::widget([
 						    'options' => ['id' => "nav-mobile", 'class' => 'right side-nav'],
@@ -85,27 +85,27 @@ AppAsset::register($this);
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="mdi-image-flash-on"></i></h2> 
-            <h5 class="center">Speeds up development</h5>
+            <h5 class="center">Быстрое решение задач</h5>
 
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <p class="light">Ваши задачи или проблемы не будут откладываться на потом, они будут решаться здесь и сейчас.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="mdi-social-group"></i></h2>
-            <h5 class="center">User Experience Focused</h5>
+            <h5 class="center">Большой опыт в области IT</h5>
 
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <p class="light">Все наши специалисты, проработавшие в данной сфере не менее 10 лет, имеют огромный опыт решения различных задач.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="mdi-action-settings"></i></h2>
-            <h5 class="center">Easy to work with</h5>
+            <h5 class="center">Передовые технологии</h5>
 
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            <p class="light">Мы работаем только с самым свежим софтом и новейшими технологиями. Классика это хорошо, но надо идти в ногу со временем.</p>
           </div>
         </div>
       </div>
@@ -149,7 +149,7 @@ AppAsset::register($this);
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text lighten-3" href="http://materializecss.com">Materialize</a>
+<!--      Made by <a class="orange-text lighten-3" href="http://materializecss.com">Materialize</a>-->
       </div>
     </div>
   </footer>  
