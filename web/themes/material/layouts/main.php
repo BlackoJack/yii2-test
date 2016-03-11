3bbcf97bce7056be30bf01f979de42496ebd0eb0
BlackoJack <billing@sibdata.ru>
2016-03-11 17:27:01 +0600

    переделал Меню

--- web/themes/material/layouts/main.php
+++ web/themes/material/layouts/main.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
   <?php $this->beginBody() ?>
   <nav class="light-blue lighten-1" role="navigation">
     <div class="container">
-      <div class="nav-wrapper"><a id="logo-container" href="/main/default/index" class="brand-logo"><?php echo Html::encode(\Yii::$app->name); ?></a>
+      <div class="nav-wrapper"><a id="logo-container" href="/" class="brand-logo"><?php echo Html::encode(\Yii::$app->name); ?></a>
 	  		<?php
 						echo Menu::widget([
 						    'options' => ['id' => "nav-mobile", 'class' => 'right side-nav'],
