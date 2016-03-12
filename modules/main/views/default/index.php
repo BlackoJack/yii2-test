622bdcc56ece8440f17368265b894a377c14a475
Раенко Иван <admin@sibservice.tk>
2016-03-12 21:53:40 +0600

    Правка текстов

--- modules/main/views/default/index.php
+++ modules/main/views/default/index.php
@@ -9,7 +9,7 @@ use yii\helpers\Html;
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1><?=Html::img('@web/images/logo.svg', ['alt'=>Yii::$app->name, 'height'=>'60%'])?></h1>
+        <h1><?=Html::img('@web/images/logo.svg', ['alt'=>Yii::$app->name, 'width'=>'60%'])?></h1>
 
         <p class="lead">IT-решение для решения ваших задач.</p>
 
